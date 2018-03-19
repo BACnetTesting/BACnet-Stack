907868062bfeb11458bc51bdce621232a36ae987
Ed <edward@connect-ex.com>
2018-03-18 21:08:27 -0700

    License correction.

--- bits/util/btaDebug.c
+++ bits/util/btaDebug.c
@@ -1,30 +1,24 @@
 /**************************************************************************
-*
-* Copyright (C) 2016 BACnet Interoperability Testing Services, Inc.
-* 
-*       <info@bac-test.com>
-*
-* Permission is hereby granted, to whom a copy of this software and
-* associated documentation files (the "Software") is provided by BACnet
-* Interoperability Testing Services, Inc., to deal in the Software
-* without restriction, including without limitation the rights to use,
-* copy, modify, merge, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included
-* in all copies or substantial portions of the Software.
-*
-* The software is provided on a non-exclusive basis.
-*
-* The permission is provided in perpetuity.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
+
+Copyright (C) 2018 BACnet Interoperability Testing Services, Inc.
+
+This program is free software : you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+
+For more information : info@bac-test.com
+For access to source code : info@bac-test.com
+or www.github.com/bacnettesting/bacnet-stack
+
 *********************************************************************/
 
 // in MSVC, winsock2.h must be included first
