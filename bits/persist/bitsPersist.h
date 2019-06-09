5bfb359f6a063c65480c737b4b95b52c31804f06
Ed <edward@connect-ex.com>
2019-06-08 18:00:13 -0700

    Removing orphan files that have moved / irrelevant to new directory structure

--- bits/persist/bitsPersist.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//void PersistRecipientAdd(int devInst, int ncInst, void *recipient);
-//bool PersistRecipientOpen(void);
-//void PersistRecipientClose(void);
-
-void PersistRecipientLists(void);
-void RestoreRecipientLists(void);
-void LoadRouterPortConfigs(void);
